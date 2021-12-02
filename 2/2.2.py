e300e863df2028f356659b8f1154a7873794ecb4
ElliottJSt <elliottjstewart@outlook.com>
2021-12-02 17:05:55 +0000

    Day 2 of Adventofcode complete

---
+++ 2/2.2.py
@@ -0,0 +1,21 @@
+
+
+#opens the data file, seperated each value by line, converts the value to an int, and saves into "data" list.
+with open("data.txt") as tmpfile:
+  data = [line.rstrip("\n") for line in tmpfile]
+
+forward = 0
+depth = 0
+aim = 0
+
+for each in data:
+  x = each.split(" ")
+  if x[0] == "forward":
+    forward += int(x[1])
+    depth += aim*int(x[1])
+  elif x[0] == "down":
+    aim += int(x[1])
+  elif x[0] == "up":
+    aim -= int(x[1])
+
+print(forward, "*", depth,"=",forward*depth)
