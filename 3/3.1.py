3481b615558c6559612ab3934a2591cb9edc0d1a
ElliottJSt <elliottjstewart@outlook.com>
2021-12-03 22:26:34 +0000

    Update 3.1.py

--- 3/3.1.py
+++ 3/3.1.py
@@ -3,24 +3,20 @@
 with open("data.txt") as tmpfile:
     data = [line.rstrip("\n") for line in tmpfile]
 
-gammaRate = "0b"  # initialises rates as binary
-epsilonRate = "0b"
-temp1 = 0
-temp0 = 0
-
+epsilonRate, gammaRate, valBalance = "", "", 0
+print(epsilonRate, gammaRate, valBalance)
 for x in range(0, len(data[0])):
     for each in data:
         if each[x] == "1":
-            temp1 += 1
-        else:
-            temp0 += 1
-    if temp1 > temp0:  # 1 most common
+            valBalance += 1
+        elif each[x] == "0":
+            valBalance -= 1
+
+    if valBalance > 0:  # 1 most common
         gammaRate += "1"
-        epsilonRate +="0"
-    elif temp0 > temp1:  # 0 most common
+        epsilonRate += "0"
+    elif valBalance < 0:  # 0 most common
         gammaRate += "0"
         epsilonRate += "1"
-    temp0 = temp1 = 0
-
-print(int(gammaRate, 2), int(epsilonRate, 2))
-print(int(gammaRate, 2) * int(epsilonRate, 2))
+    valBalance = 0
+print("Power Consumption = ", int(gammaRate, 2) * int(epsilonRate, 2))
